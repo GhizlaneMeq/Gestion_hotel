e869411f775ea1602d29b251d9cfb7eb4ebfab2d
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-09-11 14:53:15 +0100

    initial Repositories

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,26 @@
 import Entities.User;
-import Services.UserService;
+import Entities.Room;
+import Exceptions.RoomAlreadyExistException;
+import Exceptions.UserAlreadyExistsException;
+import Menus.LoginMenu;
+import Menus.ModeratorMenu;
+import Menus.UserMenu;
 
-import java.sql.SQLException;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserService();
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            User newUser = new User(1L,"John Doe", "john.doe@example.com", "123-456-7890", "password123");
-            userService.createUser(newUser);
-
-            // Retrieve all users
-            /*List<User> users = userService.getAllUsers();
-            for (User user : users) {
-                System.out.println(user);
-            }*/
-/*
-            // Update a user
-            User existingUser = userService.getUserById(1); // Example ID
-            existingUser.setPhone("987-654-3210");
-            userService.updateUser(existingUser, 1);
-
-            // Delete a user
-            userService.deleteUser(1); // Example ID
-*/
-        } catch (SQLException e) {
-            e.printStackTrace();
-            // Handle the SQL exception appropriately
+        while (true) {
+            if (LoginMenu.getCurrentUser() == null) {
+                LoginMenu.showLoginMenu(scanner);
+            } else {
+                User currentUser = LoginMenu.getCurrentUser();
+                ModeratorMenu.showUserMenu(scanner, currentUser);
+            }
         }
     }
+
+
 }
